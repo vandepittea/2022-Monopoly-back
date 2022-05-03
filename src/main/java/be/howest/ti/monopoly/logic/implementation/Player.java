dc3ce464b3276999866297c00328a9bf281e1ae3
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-04 00:30:30 +0200

    Add final to name of Player

--- src/main/java/be/howest/ti/monopoly/logic/implementation/Player.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Player.java
@@ -1,7 +1,7 @@
 package be.howest.ti.monopoly.logic.implementation;
 
 public class Player {
-    private String name;
+    private final String name;
 
     public Player(String name){
         this.name = name;
