18899e2461224ddeaf50132b00453cde680bad10
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-03 14:34:55 +0200

    add class Utility with fields and constructor

---
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/tile/Utility.java
@@ -0,0 +1,13 @@
+package be.howest.ti.monopoly.logic.implementation.tile;
+
+public class Utility extends Property {
+    private String type;
+    private String rent;
+
+    public Utility(int position, String name, int cost, int mortgage, int groupSize, String color,
+                   String type, String rent) {
+        super(position, name, cost, mortgage, groupSize, color);
+        this.type = type;
+        this.rent = rent;
+    }
+}
