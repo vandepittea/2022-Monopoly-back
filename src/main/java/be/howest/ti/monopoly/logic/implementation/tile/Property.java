d9b376e6e5f85df48c3b89ce30325cbd0f07a546
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-03 14:24:25 +0200

    add class Property with fields and constructor

---
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/tile/Property.java
@@ -0,0 +1,16 @@
+package be.howest.ti.monopoly.logic.implementation.tile;
+
+public class Property extends Tile {
+    private int cost;
+    private int mortgage;
+    private int groupSize;
+    private String color;
+
+    public Property(int position, String name, int cost, int mortgage, int groupSize, String color) {
+        super(position, name);
+        this.cost = cost;
+        this.mortgage = mortgage;
+        this.groupSize = groupSize;
+        this.color = color;
+    }
+}
