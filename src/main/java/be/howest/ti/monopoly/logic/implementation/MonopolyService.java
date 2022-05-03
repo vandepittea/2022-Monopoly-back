5f30eaaa368f80cb7d7f72983e66f735296ba3cb
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-03 14:05:07 +0200

    Add basic code for getTiles

--- src/main/java/be/howest/ti/monopoly/logic/implementation/MonopolyService.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/MonopolyService.java
@@ -2,11 +2,22 @@ package be.howest.ti.monopoly.logic.implementation;
 
 import be.howest.ti.monopoly.logic.ServiceAdapter;
 
+import java.util.List;
+
 
 public class MonopolyService extends ServiceAdapter {
 
     @Override
     public String getVersion() {
         return "0.0.1";
+    }
+
+    @Override
+    public List<Tile> getTiles(){
+        return List.of(
+                new Tile(0, "Go"),
+                new Tile(1, "Med"),
+                new Tile (2, "Community Chest")
+        );
     }
 }
