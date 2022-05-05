0bb6bf7af811f6f0a2dccf9debbb4ada2d58b6d4
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-05 17:57:20 +0200

    Add fields, constructor and getters for Player

--- src/main/java/be/howest/ti/monopoly/logic/implementation/Player.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Player.java
@@ -1,16 +1,61 @@
 package be.howest.ti.monopoly.logic.implementation;
 
+import be.howest.ti.monopoly.logic.implementation.tile.Tile;
+
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Player {
     private final String name;
+    private String currentTile;
+    private boolean jailed;
+    private int money;
+    private boolean bankrupt;
+    private int getOutOfJailCards;
+    private String taxSystem;
+    private Set<Tile> properties;
+    private int debt;
 
     public Player(String name){
         this.name = name;
+        this.currentTile = "Go";
+        this.jailed = false;
+        this.money = 1500;
+        this.bankrupt = false;
+        this.getOutOfJailCards = 0;
+        this.taxSystem = "ESTIMATE";
+        properties = new HashSet<>();
+        this.debt = 0;
     }
 
     public String getName() {
         return name;
+    }
+    public String getCurrentTile() {
+        return currentTile;
+    }
+    public boolean isJailed() {
+        return jailed;
+    }
+    public int getMoney() {
+        return money;
+    }
+    public boolean isBankrupt() {
+        return bankrupt;
+    }
+
+    public int getGetOutOfJailCards() {
+        return getOutOfJailCards;
+    }
+    public String getTaxSystem() {
+        return taxSystem;
+    }
+    public Set<Tile> getProperties() {
+        return properties;
+    }
+    public int getDebt() {
+        return debt;
     }
 
     @Override
