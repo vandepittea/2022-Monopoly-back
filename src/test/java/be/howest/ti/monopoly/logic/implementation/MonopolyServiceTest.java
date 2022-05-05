e501760f13a27d5ee1766e0bbd3e5b318b2830aa
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-05 13:06:22 +0200

    Test code for joining game and starting game

--- src/test/java/be/howest/ti/monopoly/logic/implementation/MonopolyServiceTest.java
+++ src/test/java/be/howest/ti/monopoly/logic/implementation/MonopolyServiceTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MonopolyServiceTest {
     @Test
-    void joinGameOnePlayer(){
+    void joinGameSuccesful(){
         MonopolyService s = new MonopolyService();
         Game g = s.createGame(2, "group17");
         Player p = new Player("Bob");
@@ -15,5 +15,19 @@ class MonopolyServiceTest {
 
         assertTrue(g.getPlayers().contains(p));
         assertFalse(g.isStarted());
+    }
+    @Test
+    void joinGameAndStartGame(){
+        MonopolyService s = new MonopolyService();
+        Game g = s.createGame(2, "group17");
+        Player p = new Player("Bob");
+        Player p2 = new Player("Jan");
+
+        s.joinGame("group17_0", "Bob");
+        s.joinGame("group17_0", "Jan");
+
+        assertTrue(g.getPlayers().contains(p));
+        assertTrue(g.getPlayers().contains(p2));
+        assertTrue(g.isStarted());
     }
 }
