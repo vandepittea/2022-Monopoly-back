b882cd7f3f2b0ee8bd3dda3969c6540f7d261c8c
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-06 16:27:25 +0200

    Delete clearGameList

--- src/main/java/be/howest/ti/monopoly/web/MonopolyApiBridge.java
+++ src/main/java/be/howest/ti/monopoly/web/MonopolyApiBridge.java
@@ -68,7 +68,6 @@ public class MonopolyApiBridge {
         routerBuilder.operation("getGames").handler(this::getGames);
         routerBuilder.operation("createGame").handler(this::createGame);
         routerBuilder.operation("joinGame").handler(this::joinGame);
-        routerBuilder.operation("clearGameList").handler(this::clearGameList);
 
         // Game Info
         routerBuilder.operation("getGame").handler(this::getGame);
