c8086325866728e7e375deb25c869515b06e6a34
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-06 10:27:21 +0200

    Test for failing payment when buying property

--- src/test/java/be/howest/ti/monopoly/logic/implementation/PlayerTest.java
+++ src/test/java/be/howest/ti/monopoly/logic/implementation/PlayerTest.java
@@ -1,14 +1,17 @@
 package be.howest.ti.monopoly.logic.implementation;
 
+import be.howest.ti.monopoly.logic.exceptions.IllegalMonopolyActionException;
+import be.howest.ti.monopoly.logic.exceptions.MonopolyResourceNotFoundException;
 import be.howest.ti.monopoly.logic.implementation.tile.Property;
 import be.howest.ti.monopoly.web.views.PropertyView;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
     @Test
-    void buyProperty(){
+    void buyPropertySuccesful(){
         Player pl = new Player("Bob");
         Property pr = new Property(1, "name", 5, 3, 2, "PURPLE");
         PropertyView prView = new PropertyView(pr);
@@ -17,5 +20,13 @@ class PlayerTest {
 
         assertEquals(1500 - 5, pl.getMoney());
         assertTrue(pl.getProperties().contains(prView));
+    }
+
+    @Test
+    void buyPropertyFailPayment(){
+        Player pl = new Player("Bob");
+        Property pr = new Property(1, "name", 1505, 3, 2, "PURPLE");
+
+        Assertions.assertThrows(IllegalMonopolyActionException.class, () -> pl.buyProperty(pr));
     }
 }
