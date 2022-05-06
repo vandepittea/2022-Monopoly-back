c29add1e3b46622cf8d762ae9c536cd048d07f02
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-06 09:10:02 +0200

    Optimalize constructor of MonopolyService

--- src/main/java/be/howest/ti/monopoly/logic/implementation/MonopolyService.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/MonopolyService.java
@@ -18,51 +18,22 @@ import java.util.Set;
 
 public class MonopolyService extends ServiceAdapter {
     private Set<Game> games;
-    private List<Tile> tiles;
-    private String[] chances;
-    private String[] communityChests;
+    private final List<Tile> tiles;
+    private final String[] chances;
+    private final String[] communityChests;
 
     public MonopolyService() {
         games = new HashSet<>();
 
-        chances = new String[]{
-                "Advance to Boardwalk",
-                "Advance to Go (Collect $200)",
-                "Advance to Illinois Avenue. If you pass Go, collect $200",
-                "Advance to St. Charles Place. If you pass Go, collect $200",
-                "Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled",
-                "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown.",
-                "Bank pays you dividend of $50",
-                "Get Out of Jail Free",
-                "Go Back 3 Spaces",
-                "Go to Jail. Go directly to Jail, do not pass Go, do not collect $200",
-                "Make general repairs on all your property. For each house pay $25. For each hotel pay $100",
-                "Speeding fine $15",
-                "Take a trip to Reading Railroad. If you pass Go, collect $200",
-                "You have been elected Chairman of the Board. Pay each player $50",
-                "Your building loan matures. Collect $150"
-        };
+        chances = generateChances();
 
-        communityChests = new String[]{
-                "Advance to Go (Collect $200)",
-                "Bank error in your favor. Collect $200",
-                "Doctor's fee. Pay $50",
-                "From sale of stock you get $50",
-                "Get Out of Jail Free",
-                "Go to Jail. Go directly to jail, do not pass Go, do not collect $200",
-                "Holiday fund matures. Receive $100",
-                "Income tax refund. Collect $20",
-                "It is your birthday. Collect $10 from every player",
-                "Life insurance matures. Collect $100",
-                "Pay hospital fees of $100 ",
-                "Pay school fees of $50",
-                "Receive $25 consultancy fee ",
-                "You are assessed for street repair. $40 per house. $115 per hotel",
-                "You have won second prize in a beauty contest. Collect $10",
-                "You inherit $100"
-        };
+        communityChests = generateCommunityChests();
 
         tiles = new ArrayList<>();
+        generateTiles();
+    }
+
+    private void generateTiles(){
         tiles.add(new SimpleTile(0, "Peach Castle", "Go"));
         tiles.add(new Street(1, "Peach's Garden", 60, 30, 2, "PURPLE", new Integer[]{10, 30, 90, 160, 250}, 50, "PURPLE", 2));
         tiles.add(new CardExecutingTile(2, "Community Chest I", "community chest"));
@@ -103,6 +74,45 @@ public class MonopolyService extends ServiceAdapter {
         tiles.add(new Street(37, "Crumbleden", 350, 175, 2, "DARKBLUE", new Integer[]{175, 500, 1100, 1300, 1500}, 200, "DARKGREEN", 35));
         tiles.add(new CardExecutingTile(38, "Luxury Tax", "Luxury Tax"));
         tiles.add(new Street(39, "Bowser Castle", 400, 200, 2, "DARKBLUE", new Integer[]{200, 600, 1400, 1700, 2000}, 200, "DARKBLUE", 50));
+    }
+
+    private String[] generateChances(){
+        return new String[]{ "Advance to Boardwalk",
+                "Advance to Go (Collect $200)",
+                "Advance to Illinois Avenue. If you pass Go, collect $200",
+                "Advance to St. Charles Place. If you pass Go, collect $200",
+                "Advance to the nearest Railroad. If unowned, you may buy it from the Bank. If owned, pay owner twice the rental to which they are otherwise entitled",
+                "Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times amount thrown.",
+                "Bank pays you dividend of $50",
+                "Get Out of Jail Free",
+                "Go Back 3 Spaces",
+                "Go to Jail. Go directly to Jail, do not pass Go, do not collect $200",
+                "Make general repairs on all your property. For each house pay $25. For each hotel pay $100",
+                "Speeding fine $15",
+                "Take a trip to Reading Railroad. If you pass Go, collect $200",
+                "You have been elected Chairman of the Board. Pay each player $50",
+                "Your building loan matures. Collect $150"
+        };
+    }
+
+    private String[] generateCommunityChests(){
+        return new String[]{ "Advance to Go (Collect $200)",
+                "Bank error in your favor. Collect $200",
+                "Doctor's fee. Pay $50",
+                "From sale of stock you get $50",
+                "Get Out of Jail Free",
+                "Go to Jail. Go directly to jail, do not pass Go, do not collect $200",
+                "Holiday fund matures. Receive $100",
+                "Income tax refund. Collect $20",
+                "It is your birthday. Collect $10 from every player",
+                "Life insurance matures. Collect $100",
+                "Pay hospital fees of $100 ",
+                "Pay school fees of $50",
+                "Receive $25 consultancy fee ",
+                "You are assessed for street repair. $40 per house. $115 per hotel",
+                "You have won second prize in a beauty contest. Collect $10",
+                "You inherit $100"
+        };
     }
 
     @Override
