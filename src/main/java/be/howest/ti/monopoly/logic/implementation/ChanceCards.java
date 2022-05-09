b41f5e28d057c8027888c1e23c14948373b771b9
Tim De Jonghe <tim.de.jonghe@student.howest.be>
2022-05-09 15:28:19 +0200

    Made chance/community enums

---
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/ChanceCards.java
@@ -0,0 +1,19 @@
+package be.howest.ti.monopoly.logic.implementation;
+
+public enum ChanceCards {
+    ADV_BOARDWALK,
+    ADV_GO,
+    ADV_ILL,
+    ADV_CHARLES,
+    ADV_RR,
+    ADV_UT,
+    REC_BANK_50,
+    JAIL_CARD,
+    RETURN_3,
+    GO_JAIL,
+    PAY_REPAIRS,
+    PAY_15,
+    GO_READING,
+    PAY_PLAYERS_50,
+    REC_150
+}
---
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/CommunityChests.java
@@ -0,0 +1,20 @@
+package be.howest.ti.monopoly.logic.implementation;
+
+public enum CommunityChests {
+    ADV_GO,
+    REC_BANK_ERR,
+    PAY_DOCTOR_FEE,
+    REC_SOLD_STOCK,
+    GET_JAIL_CARD,
+    GO_JAIL,
+    REC_HOLIDAY,
+    REC_TAX,
+    REC_BIRTHDAY,
+    REC_INSURANCE,
+    PAY_HOSPITAL,
+    PAY_SCHOOL,
+    REC_CONSULTANCY,
+    PAY_REPAIRS,
+    REC_PRIZE,
+    REC_INHERIT
+}
