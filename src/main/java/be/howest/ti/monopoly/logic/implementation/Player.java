231ff58a748b51bcc54c956dd4afea9b1760db51
Alexander Vandepitte <alexander.vandepitte@student.howest.be>
2022-05-09 16:26:07 +0200

    Add a house to a street and pay for the house

--- src/main/java/be/howest/ti/monopoly/logic/implementation/Player.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/Player.java
@@ -194,7 +194,19 @@ public class Player {
         if(checkOwnershipWholeStreet(s, service)){
             throw new IllegalMonopolyActionException("You can only build on a property when you own the whole group.");
         }
+        if(s.checkStreetHouseDifference(service)){
+            throw new IllegalMonopolyActionException("The difference between the houses in a street should " +
+                    "not be higher than one.");
+        }
+        else{
+            boolean successfulPayment = payMoney(s.getHousePrice());
 
+            if (successfulPayment) {
+                s.buyHouse();
+            } else {
+                throw new IllegalMonopolyActionException("You don't have enough money to buy a house for this property.");
+            }
+        }
     }
 
     private boolean checkOwnershipWholeStreet(Street streetToBuildHouseOn, MonopolyService service){
