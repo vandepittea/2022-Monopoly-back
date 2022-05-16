94b5edf301ca12c70619ab9bfb6d72785117d61b
Tim De Jonghe <tim.de.jonghe@student.howest.be>
2022-05-16 16:32:03 +0200

    Refactored enums

--- src/main/java/be/howest/ti/monopoly/logic/implementation/enums/TurnType.java
+++ src/main/java/be/howest/ti/monopoly/logic/implementation/enums/TurnType.java
@@ -1,7 +1,9 @@
 package be.howest.ti.monopoly.logic.implementation.enums;
 
 public enum TurnType {
-    DEFAULT, GO_TO_JAIL, JAIL_STAY;
+    DEFAULT,
+    GO_TO_JAIL,
+    JAIL_STAY;
 
     @Override
     public String toString(){
